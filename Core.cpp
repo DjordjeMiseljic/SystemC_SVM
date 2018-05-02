65656f4908c91f4d197d4e7f24a571efe60283a7
fouste <djordjemiseljic@gmail.com>
2018-05-02 23:07:16 +0200

    Changes made to Core & main

--- Core.cpp
+++ Core.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 
-Core::Core(sc_module_name name, const int sv_num, const int sv_len, sc_event *e_ready, sc_event *e_next, int* lambda, int* tau, deque<int> *data):
+Core::Core(sc_module_name name, const int sv_num, const int sv_len, sc_event *e_ready, sc_event *e_next, int& lambda, int& tau, deque<double> &data):
 	sv_num(sv_num),
 	sv_len(sv_len),
 	e_ready(e_ready),
@@ -13,18 +13,37 @@ Core::Core(sc_module_name name, const int sv_num, const int sv_len, sc_event *e_
 {
 	cout<<"Core constucted"<<endl;
 	SC_THREAD(proc);
+	acc=0;
+
 }
 
 void Core::proc()
 {
-	int i=0;
-	while(i<10)
+	int k=sv_num;
+	long double p;
+
+	e_next->notify(SC_ZERO_TIME);
+	wait(*e_ready);
+	//copy inputs to y 
+	for(int i=0;i<sv_len;i++)
+		y.push_back(data[i]);
+
+	while(k)
 	{
-		wait(10,SC_NS);
-		cout<<"doot"<<"@ "<<sc_time_stamp()<<" #"<<name()<<endl;
-		i++;
+		p=0;
+		e_next->notify(SC_ZERO_TIME);
+		wait(*e_ready);
+		for(int i=0; i<sv_len; i++)
+			p+=(y[i]*data[i]);
+		p=p*p*p;
+		p=(lambda)*p;
+		p=(tau)*p;
+		acc+=p;
+		
+		cout<<"p= "<<p<<"\tacc= "<<acc<<"\t@ "<<sc_time_stamp()<<"\t#"<<name()<<endl;
+		k--;
 	}
-	cout<<"ending"<<endl;
+	cout<<"classification finished\nacc= "<<acc<<endl;
 	return;	
 }
 
