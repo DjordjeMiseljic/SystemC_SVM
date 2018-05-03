a005740be58444b3afe19285f225250a506e9dba
nikola <nikola2444@yahoo.com>
2018-05-03 20:41:55 +0200

    memctrl complited but segmentation faulth appears

--- MemCtrl.hpp
+++ MemCtrl.hpp
@@ -10,21 +10,21 @@ using namespace sc_core;
 
 SC_MODULE(MemCtrl)
 {
-	public:
-		SC_HAS_PROCESS(MemCtrl);
-		MemCtrl(sc_module_name, int&, const int,
-			  sc_event*, sc_event*, int&, int&, deque<double>&);
-		int num_of_sv();
-	protected:
-		int &sv_num;
-	  	int &sv_len; 
-		int &lambda;
-	   int &target;
-		sc_event  *e_ready
-		sc_event	 *e_next;
-		deque <double> &data;
-		//METHODS
-		void grab_from_mem();
+   public:
+      SC_HAS_PROCESS(MemCtrl);
+      MemCtrl(sc_module_name, int&, const int,
+	      sc_event*, sc_event*, double&, int&, deque<double>&);
+      int num_of_sv();
+   protected:
+      int &sv_num;
+      const int &sv_len; 
+      double &lambda;
+      int &target;
+      sc_event  *e_ready;
+      sc_event  *e_next;
+      deque <double> &data;
+      //METHODS
+      void grab_from_mem();
 
   
 };
--- mainSVM.cpp
+++ mainSVM.cpp
@@ -2,14 +2,14 @@
 #include <string>
 #include <deque>
 #include "Core.hpp"
-//#include "MemCtrl.hpp"
+#include "MemCtrl.hpp"
 using namespace std;
 using namespace sc_core;
 
 int sc_main(int argc, char* argv[])
 {
 	Core *acc_core;
-	//MemCtrl *mem_ctrl;
+	MemCtrl *Mem_Ctrl;
 	int sv_num = 500;
 	const int sv_len = 784;
    sc_event e_ready; // mem_ctrl->core
@@ -19,11 +19,11 @@ int sc_main(int argc, char* argv[])
 	int target=1;
 
 	acc_core = new Core("acc_core",sv_num, sv_len, &e_ready, &e_next, lambda, target, data);
-	//mem_ctrl = new MemCtrl("mem_ctrl",sv_num, sv_len, &e_ready, &e_next, lambda, target, data);
+	Mem_Ctrl = new MemCtrl("mem_ctrl",sv_num, sv_len, &e_ready, &e_next, lambda, target, data);
 
 	sc_start(200, sc_core::SC_NS);
 	cout << "Simulation finished at " << sc_time_stamp() << std::endl;
 	delete acc_core;
-	//delete mem_ctrl;	
+	delete Mem_Ctrl;	
 	return 0;
 }
