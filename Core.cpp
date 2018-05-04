34095db540d1ad1c5c3a937089b906502496cf11
fouste <djordjemiseljic@gmail.com>
2018-05-04 21:59:00 +0200

    debug

--- Core.cpp
+++ Core.cpp
@@ -19,35 +19,49 @@ Core::Core(sc_module_name name, int& sv_num, int sv_len,
 void Core::proc()
 {
    int k;
-   double p;
+   long double p;
    double b;
+   double tmp;
    e_next->notify(SC_ZERO_TIME);
    wait(*e_ready);
    k=sv_num;
    b=lambda;
    cout<<"BIAS:"<<b<<endl;
    //copy inputs to y 
+   //cout<<"Y: ";
    for(int i=0;i<sv_len;i++)
+   {
       y.push_back(data[i]);
+      //cout<<"\t"<<data[i];
+   }
+   cout<<endl;
    while(k)
       {
          cout<<"for k= "<<k<<" : "<<endl;
-         p=1;
          e_next->notify(SC_ZERO_TIME);
          wait(*e_ready);
+         //cout<<"tmp=";
+         p=1.0;
          for(int i=0; i<sv_len; i++)
-            p+=(y[i]*data[i]);
-         cout<<"   pdot:"<<p;
+         {
+            tmp=y[i]*data[i];
+            p+=tmp;
+            //cout<<" "<<y[i];
+         }
+         cout<<"\n   pdot:"<<p;
+         
          p=p*p*p;
          cout<<"   pcube:"<<p;
+         
          p=(lambda)*p;
          cout<<"   plambda:"<<p<<endl;
+         
          p=(target)*p;
+        
          wait(1,SC_NS);
          
-         printf("   current acc= %1.5e    p= %1.5e    new acc= %1.5e    ",acc,p,(acc+p));
-         cout<<"@"<<sc_time_stamp()<<"\t#"<<name()<<endl;
-         //cout<<"\tcurrent acc= "<<acc<<"\tp= "<<p<<"\t@ "<<sc_time_stamp()<<"\t#"<<name()<<endl;
+         cout<<"\tcurrent acc="<<acc<<"\tp="<<p<<"\tnew acc="<<(acc+p);
+         cout<<"\t@"<<sc_time_stamp()<<"\t#"<<name()<<endl;
          
          acc+=p;
          k--;
