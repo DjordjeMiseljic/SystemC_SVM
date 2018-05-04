32c821abb3fb4248d938e2f08a94086ad4e72bdd
nikola <nikola2444@yahoo.com>
2018-05-04 11:59:28 +0200

    MemCtrl fixed probably

--- MemCtrl.cpp
+++ MemCtrl.cpp
@@ -13,6 +13,7 @@ MemCtrl::MemCtrl(sc_module_name name, int& sv_num, const int sv_len,
 						   target(target),
 						   data(data)
 {
+   cout<<"mem_ctrl contructed"<<endl;
    SC_THREAD(grab_from_mem);
 }
 void MemCtrl::grab_from_mem()
@@ -23,66 +24,78 @@ void MemCtrl::grab_from_mem()
    string l_line;
    int i=0;
    while(1)
-      {
-	 data.clear();
-	 wait(*e_next);
-	 sv_num = num_of_sv();
-	 string y_line;
-	 ifstream y_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/test_images/y.txt");
-	 ifstream sv_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/support_vectors/sv0.txt");
-	 ifstream l_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/lambdas/lambdas0.txt");
-	 ifstream t_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/targets/target0.txt");
-
+   {
 	 
-	 for(int i=0; i<sv_len; i++)
-	    {
-	       if(getline(y_file, y_line, ' '))
-		  data.push_back(stof(y_line));
-	       else
-		  cout<<"couldn't open y_file"<<endl;
-	    }
-	 e_ready->notify(SC_ZERO_TIME);
-	 while(i<sv_num)
-	    {
+      wait(*e_next);
+      cout<<"first e_next received"<<endl;
+      data.clear();
+      sv_num = num_of_sv();
+      cout<<sv_num<<endl;
+      ifstream y_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/test_images/y.txt");
+      ifstream sv_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/support_vectors/sv0.txt");
+      ifstream l_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/lambdas/lambdas0.txt");
+      ifstream t_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/targets/targets0.txt");
+      if(!t_file.is_open())
+	 cout<<"sv_not openede"<<endl;
+      if(sv_file.is_open() && y_file.is_open() && l_file.is_open() && t_file.is_open())
+      {
 
-	       wait(*e_next);
-	       data.clear();
-	       for(int j = 0; j<sv_len; j++)
-		  {
-		     if(getline(sv_file, sv_line, ' '))
-			data.push_back(stof(sv_line));
-		     else
-			cout<<"couldnt open sv_file"<<endl;
-		  }
-	       if(getline(l_file, l_line))
-		  lambda = stof(l_line);
-	       else
-		  cout<<"couldnt open lambda_file"<<endl;
-	       if(getline(t_file, t_line))
-		  target = stof(t_line);
-	       else
-		  cout<<"couldnt open target_file"<<endl;
-	       e_ready->notify(SC_ZERO_TIME);
+	 for(int i=0; i<784; i++)
+	 {
+	    getline(y_file, y_line, ' ');
+	    cout<<y_line<<endl;
+	    data.push_back(stof(y_line));
+	 }
+	 e_ready->notify(SC_ZERO_TIME);
+	 cout<<"first e_ready sent"<<endl;
+	 
+	 cout<<data.size()<<endl;
+	 while(i<sv_num)
+	 {
+
+	    wait(*e_next);
+	    cout<<"e_next received"<<endl;
+	    data.clear();
+	    for(int j = 0; j<sv_len; j++)
+	    {
+	       getline(sv_file, sv_line, ' ');
+	       data.push_back(stof(sv_line));
 	    }
-	 y_file.close();
-	 sv_file.close();
-	 l_file.close();
-	 t_file.close();
-      
+
+	    getline(l_file, l_line);
+	    lambda = stof(l_line);
+	    
+	    getline(t_file, t_line);
+	    target = stof(t_line);
+	    i++;
+	    e_ready->notify(SC_ZERO_TIME);
+	 }
       }
+      else
+      {
+	 cout<<"couldn't open one of the files"<<endl;
+	 return;
+      }
+      y_file.close();
+      sv_file.close();
+      l_file.close();
+      t_file.close();
+      
+   }
   
 }
 int MemCtrl::num_of_sv()
+
 {
    int count;
    string line;
    ifstream sv_file("/home/nikola/Documents/git_folders/ML_number_recognition_SVM/saved_data/support_vectors/sv0.txt");
    if(sv_file.is_open())
-      {
-	 while(getline(sv_file,line))
-	    count++;
-	 sv_file.close();
-      }
+   {
+      while(getline(sv_file,line))
+	 count++;
+      sv_file.close();
+   }
    else
       cout<<"error opening support vector file"<<endl;
    return count;
