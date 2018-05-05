7429ba711db326c08aa82d642dca91c3d871c96b
fouste <djordjemiseljic@gmail.com>
2018-05-05 15:35:14 +0200

    image_num++, added event queue in main

--- mainSVM.cpp
+++ mainSVM.cpp
@@ -12,15 +12,15 @@ int sc_main(int argc, char* argv[])
 	MemCtrl *Mem_Ctrl;
 	int sv_num = 500;
 	const int sv_len = 784;
-   sc_event e_ready; // mem_ctrl->core
-   sc_event e_next;	// core->mem_ctrl
+   sc_event e_ready[10]; // mem_ctrl->core
+   sc_event e_next[10];	// core->mem_ctrl
 	deque <double> data;
 	double lambda=1;
 	int target=1;
    double res=0;
 
-	acc_core = new Core("acc_core",sv_num, sv_len, &e_ready, &e_next, lambda, target, data, res);
-	Mem_Ctrl = new MemCtrl("mem_ctrl",sv_num, sv_len, &e_ready, &e_next, lambda, target, data, res);
+	acc_core = new Core("acc_core",sv_num, sv_len, &e_ready[0], &e_next[0], lambda, target, data, res);
+	Mem_Ctrl = new MemCtrl("mem_ctrl",sv_num, sv_len, &e_ready[0], &e_next[0], lambda, target, data, res);
 
 	sc_start(2000, sc_core::SC_NS);
 	cout << "Simulation finished at " << sc_time_stamp() << std::endl;
