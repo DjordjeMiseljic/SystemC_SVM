e7c7d4665d6bc86841933e407bee15a8292d6ae8
fouste <djordjemiseljic@gmail.com>
2018-05-05 16:06:01 +0200

    main changed

--- mainSVM.cpp
+++ mainSVM.cpp
@@ -1,14 +1,14 @@
 #include <systemc>
 #include <string>
 #include <deque>
-#include "Core.hpp"
+#include "Classificator.hpp"
 #include "MemCtrl.hpp"
 using namespace std;
 using namespace sc_core;
 
 int sc_main(int argc, char* argv[])
 {
-	Core *acc_core;
+	Classificator *Cl;
 	MemCtrl *Mem_Ctrl;
 	int sv_num = 500;
 	const int sv_len = 784;
@@ -19,12 +19,12 @@ int sc_main(int argc, char* argv[])
 	int target=1;
    double res=0;
 
-	acc_core = new Core("acc_core",sv_num, sv_len, &e_ready[0], &e_next[0], lambda, target, data, res);
-	Mem_Ctrl = new MemCtrl("mem_ctrl",sv_num, sv_len, &e_ready[0], &e_next[0], lambda, target, data, res);
+	Cl = new Classificator("acc_core",sv_num, sv_len, e_ready, e_next, lambda, target, data, res);
+	Mem_Ctrl = new MemCtrl("mem_ctrl",sv_num, sv_len, e_ready, e_next, lambda, target, data, res);
 
 	sc_start(2000, sc_core::SC_NS);
 	cout << "Simulation finished at " << sc_time_stamp() << std::endl;
-	delete acc_core;
+	delete Cl;
 	delete Mem_Ctrl;	
 	return 0;
 }
