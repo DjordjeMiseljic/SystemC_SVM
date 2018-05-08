aacc9ae402218dd0fa97e697d14a0a9fcce99f47
fouste <djordjemiseljic@gmail.com>
2018-05-08 17:48:08 +0200

    Types

--- Types.hpp
+++ Types.hpp
@@ -1,8 +1,10 @@
-#include <sc_dt>
+#include <systemc>
 #define W   16
 #define Q   SC_RND
 #define O   SC_SAT_SYM
+using namespace sc_dt;
 
 typedef sc_fixed_fast <W,2,Q,O> din_t;
-typedef sc_fixed_fast <W,4,Q,O> res_t;
-typedef sc_fixed_fast <4,4,Q,O> num_t;
+typedef sc_fixed_fast <W,4,Q,O> acc_t;
+typedef sc_fixed_fast <(2*W),10,Q,O> p_t;
+typedef sc_ufixed_fast <4,4,Q,O> num_t;
