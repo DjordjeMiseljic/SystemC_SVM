68b2e72a83983528ed48080cbb0c1e4d9f0ed178
nikola <nikola2444@yahoo.com>
2018-05-20 10:57:33 +0200

    in mainSvm indentations fixed

--- Pre_TLM/mainSVM.cpp
+++ Pre_TLM/mainSVM.cpp
@@ -12,31 +12,31 @@ using namespace sc_core;
 
 int sc_main(int argc, char* argv[])
 {
-	Classificator *Cl;
-	MemCtrl *Mem_Ctrl;
+   Classificator *Cl;
+   MemCtrl *Mem_Ctrl;
    Checker *Check;
    
    num_t number; 
-	int sv_num = 500;
-	const int sv_len = 784;
+   int sv_num = 500;
+   const int sv_len = 784;
    sc_event e_ready[10];
    sc_event e_next[10];
    sc_event e_fin;
-	deque <din_t> data;
-	lin_t lambda;
-	bin_t bias;
+   deque <din_t> data;
+   lin_t lambda;
+   bin_t bias;
    res_t res;
    
-	Cl = new Classificator("classificator",sv_num, sv_len, e_ready, e_next, &e_fin, lambda, bias, data, res, number);
-	Mem_Ctrl = new MemCtrl("mem_ctrl",sv_num, sv_len, e_ready, e_next, lambda, bias, data);
+   Cl = new Classificator("classificator",sv_num, sv_len, e_ready, e_next, &e_fin, lambda, bias, data, res, number);
+   Mem_Ctrl = new MemCtrl("mem_ctrl",sv_num, sv_len, e_ready, e_next, lambda, bias, data);
    Check = new Checker("checker", &e_fin, res, number);
 
-	sc_start(20, sc_core::SC_SEC);
-	cout << "Simulation finished at " << sc_time_stamp() << std::endl;
+   sc_start(20, sc_core::SC_SEC);
+   cout << "Simulation finished at " << sc_time_stamp() << std::endl;
    
-	delete Cl;
-	delete Mem_Ctrl;	
+   delete Cl;
+   delete Mem_Ctrl;	
    delete Check;
 
-	return 0;
+   return 0;
 }
